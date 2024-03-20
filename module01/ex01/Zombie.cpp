aefbb6938fdc54d2fe65c5f8261f1c5fee91e009
Harsh Kumbhani <Harshwut@icloud.com>
2024-03-20 09:47:17 +0100

    module01 ex01

---
+++ module01/ex01/Zombie.cpp
@@ -0,0 +1,22 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() {
+	std::cout << "Default Zombie" << ": is Born" << std::endl;
+}
+
+Zombie::Zombie(std::string name) : name(name) {}
+
+Zombie::~Zombie(void){
+	std::cout << this->name << ": Died" << std::endl;
+	return ;
+}
+
+void	Zombie::announce(void) const{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	return ;
+}
+
+void	Zombie::setName(std::string name)
+{
+	this->name = name;
+}
---
+++ module01/ex01/main.cpp
@@ -0,0 +1,25 @@
+#include "Zombie.hpp"
+
+#define N 5
+
+int	main(void)
+{
+	Zombie*   horde;
+
+	std::cout << "=================================================" << std::endl;
+	std::cout << "||             ~ Zombie Horde Awakens ~        ||" << std::endl;
+	std::cout << "=================================================" << std::endl;
+	horde = zombieHorde(N, "Crazy");
+	std::cout << "=================================================" << std::endl;
+	std::cout << "||           ~ Announcing The Horde ~          ||" << std::endl;
+	std::cout << "=================================================" << std::endl;
+	for (int i = 0; i < N; i++) {
+		std::cout << "=> Zombie #" << i + 1 << " steps forward:" << std::endl;
+		horde[i].announce();
+	}
+	std::cout << "=================================================" << std::endl;
+	std::cout << "||             ~ Disbanding The Horde ~        ||" << std::endl;
+	std::cout << "=================================================" << std::endl;
+	delete [] horde;
+	return EXIT_SUCCESS;
+}
