538951ac817d0798ea78684857113224e3320d70
Harsh Kumbhani <Harshwut@icloud.com>
2024-03-23 14:43:56 +0100

    Fixed error message on empty input field

--- module00/ex01/Contact.cpp
+++ module00/ex01/Contact.cpp
@@ -44,8 +44,23 @@ void Contact::create_new_entry() {
                 << " Phone Number." << std::endl;
           isValid = false;
         } } else {
+        std::string::iterator it;
         isValid = input.empty() == false
           && input.find_last_not_of(" \n\t\v\f\r") != std::string::npos;
+        if (input.compare("\"\"") == 0){
+          isValid = false;
+          std::cout << fields[i] << " cannot be an empty." << std::endl;
+        }
+        else if (input[0] == '\"' && input[input.length() - 1] == '\"'){
+          input = input.substr(1, input.length() - 2);
+        }else {
+        for(it = input.begin(); it != input.end(); it++){
+          if (*it == '\"'){
+            std::cout << "Illegal use of \" character detected or trailing"
+                << " spaces / characters after \"" << std::endl;
+            isValid = false;
+            break ;
+        }}}
       }
     } while (isValid == false);
     *data[i] = input;
