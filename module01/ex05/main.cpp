26bc2a4d6e0d64374037f17181d94f887979a223
Harsh Kumbhani <Harshwut@icloud.com>
2024-03-25 08:59:21 +0100

    Indentations fix

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -1,15 +1,14 @@
 #include "Harl.hpp"
 
-int	main(int argc, char *argv[])
-{
-	Harl	harl;
+int main(int argc, char *argv[]) {
+  Harl harl;
 
-	if (argc != 2)
-	{
-		std::cerr << "Usage: " << argv[0] << "    < DEBUG | INFO | WARNING | ERROR >" << std::endl;
-		return (1);
-	}
-	to_upper(argv[1]);
-	harl.complain(argv[1]);
-	return (0);
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0]
+              << "    < DEBUG | INFO | WARNING | ERROR >" << std::endl;
+    return (1);
+  }
+  to_upper(argv[1]);
+  harl.complain(argv[1]);
+  return (0);
 }
