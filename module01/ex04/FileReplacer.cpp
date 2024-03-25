f3e705995bc1bc44b3589f0585d3dd1ca5bfc001
Harsh Kumbhani <Harshwut@icloud.com>
2024-03-25 08:51:57 +0100

    Indentations fixed

--- module01/ex04/FileReplacer.cpp
+++ module01/ex04/FileReplacer.cpp
@@ -1,49 +1,47 @@
 #include "FileReplacer.hpp"
 
-FileReplacer::FileReplacer(std::string filename, std::string searchStr, std::string replaceStr) :
-_filename(filename), _searchStr(searchStr), _replaceStr(replaceStr) {}
+FileReplacer::FileReplacer(std::string filename, std::string searchStr,
+                           std::string replaceStr)
+    : _filename(filename), _searchStr(searchStr), _replaceStr(replaceStr) {}
 
 FileReplacer::~FileReplacer() {}
 
-void	FileReplacer::replace(){
-	
-	std::ifstream inputFile(_filename.c_str());
-	if (inputFile.fail() == true)
-	{
-		std::cerr << "Could not open the input file: " << _filename << std::endl;
-		return ;
-	}
-	if (_searchStr.empty() == true)
-	{
-		std::cerr << "Search string cannot be empty" << std::endl;
-		return ;
-	}
-	
-	std::ofstream outputFile((_filename + ".replace").c_str());
-	if (outputFile.fail() == true)
-	{
-		std::cerr << "Could not create output file: " << _filename + ".replace" << std::endl;
-		return ;
-	}
-	_buff.assign(std::istreambuf_iterator<char>(inputFile), std::istreambuf_iterator<char>());
-	_outfile = replaceOccurence(_buff);
-	outputFile << _outfile;
-	inputFile.close();
-	outputFile.close();
-	return ;
+void FileReplacer::replace() {
+
+  std::ifstream inputFile(_filename.c_str());
+  if (inputFile.fail() == true) {
+    std::cerr << "Could not open the input file: " << _filename << std::endl;
+    return;
+  }
+  if (_searchStr.empty() == true) {
+    std::cerr << "Search string cannot be empty" << std::endl;
+    return;
+  }
+
+  std::ofstream outputFile((_filename + ".replace").c_str());
+  if (outputFile.fail() == true) {
+    std::cerr << "Could not create output file: " << _filename + ".replace"
+              << std::endl;
+    return;
+  }
+  _buff.assign(std::istreambuf_iterator<char>(inputFile),
+               std::istreambuf_iterator<char>());
+  _outfile = replaceOccurence(_buff);
+  outputFile << _outfile;
+  inputFile.close();
+  outputFile.close();
+  return;
 }
 
-std::string	FileReplacer::replaceOccurence(const std::string &source)
-{
-	std::string	result;
-	size_t		lastPos = 0, findPos;
+std::string FileReplacer::replaceOccurence(const std::string &source) {
+  std::string result;
+  size_t lastPos = 0, findPos;
 
-	while ((findPos = source.find(_searchStr, lastPos)) != std::string::npos)
-	{
-		result.append(source, lastPos, findPos - lastPos);
-		result += _replaceStr;
-		lastPos = findPos + _searchStr.length();
-	}
-	result += source.substr(lastPos);
-	return result;
+  while ((findPos = source.find(_searchStr, lastPos)) != std::string::npos) {
+    result.append(source, lastPos, findPos - lastPos);
+    result += _replaceStr;
+    lastPos = findPos + _searchStr.length();
+  }
+  result += source.substr(lastPos);
+  return result;
 }
--- module01/ex04/FileReplacer.hpp
+++ module01/ex04/FileReplacer.hpp
@@ -1,24 +1,24 @@
 #ifndef FILEREPLACER_H
-# define FILEREPLACER_H
+#define FILEREPLACER_H
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
-class	FileReplacer
-{
-	private:
-		std::string	_filename;
-		std::string	_searchStr;
-		std::string	_replaceStr;
-		std::string	_buff;
-		std::string	_outfile;
-		std::string replaceOccurence(const std::string &source);
+class FileReplacer {
+private:
+  std::string _filename;
+  std::string _searchStr;
+  std::string _replaceStr;
+  std::string _buff;
+  std::string _outfile;
+  std::string replaceOccurence(const std::string &source);
 
-	public:
-		FileReplacer(std::string filename, std::string searchStr, std::string replaceStr);
-		~FileReplacer();
-		void	replace();
+public:
+  FileReplacer(std::string filename, std::string searchStr,
+               std::string replaceStr);
+  ~FileReplacer();
+  void replace();
 };
 
 #endif
