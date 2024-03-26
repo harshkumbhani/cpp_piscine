537241e6b8d360f8307fd19877ad45ffaaa3b4d5
Harsh Kumbhani <Harshwut@icloud.com>
2024-03-26 09:06:48 +0100

    module02 00

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -1,30 +1,31 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() : _fixedPointNumber(0) {
-	std::cout << "Default constructor called" << std::endl;
+  std::cout << GREEN << "⚙️  Default constructor called" << RESET << std::endl;
 }
 
-Fixed::Fixed(Fixed const &copy) : _fixedPointNumber(copy._fixedPointNumber){
-	std::cout << "Copy constructor called" << std::endl;
+Fixed::Fixed(Fixed const &src) {
+  std::cout << YELLOW << "📋 Copy constructor called" << RESET << std::endl;
+	*this = src;
 }
 
-Fixed& Fixed::operator=(const Fixed& rhs) {
-	std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &rhs)
-		_fixedPointNumber = rhs.getRawBits();
-	return *this;
+Fixed &Fixed::operator=(const Fixed &rhs) {
+  std::cout << BLUE << "➡️  Copy assignment operator called" << RESET << std::endl;
+  if (this != &rhs)
+    _fixedPointNumber = rhs.getRawBits();
+  return *this;
 }
 
-Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
+Fixed::~Fixed() { 
+	std::cout << RED << "💥 Destructor called" << RESET << std::endl;
 }
 
-int	Fixed::getRawBits(void) const {
-	std::cout << "getRawbits member function called" << std::endl;
-	return _fixedPointNumber;
+int Fixed::getRawBits(void) const {
+  std::cout << MAGENTA << "🔍 getRawbits member function called" << RESET << std::endl;
+  return _fixedPointNumber;
 }
 
-void	Fixed::setRawBits(const int raw) {
-	std::cout << "setRawbits memeber function called" << std::endl;
-	_fixedPointNumber = raw;
+void Fixed::setRawBits(const int raw) {
+  std::cout << MAGENTA << "✏️  setRawbits memeber function called" << RESET <<  std::endl;
+  _fixedPointNumber = raw;
 }
--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -1,21 +1,29 @@
 #ifndef FIXED_HPP
-# define FIXED_HPP
+#define FIXED_HPP
 
 #include <iostream>
 
+const std::string RESET = "\033[0m";
+const std::string RED =  "\033[0;31m";
+const std::string YELLOW = "\033[0;33m";
+const std::string BLUE = "\033[0;34m";
+const std::string MAGENTA = "\033[0;35m";
+const std::string CYAN = "\033[0;36m";
+const std::string GREEN = "\033[32m";
+
 class Fixed {
-	private:
-	int	_fixedPointNumber;
-	static const int	_fractionalBit = 8;
+private:
+  int _fixedPointNumber;
+  static const int _fractionalBit = 8;
 
-	public:
-	Fixed();
-	Fixed(Fixed const &);
-	Fixed& operator=(const Fixed&);
-	~Fixed();
+public:
+  Fixed();
+  Fixed(Fixed const & src);
+  Fixed &operator=(const Fixed &rhs);
+  ~Fixed();
 
-	int	getRawBits(void) const;
-	void	setRawBits(int const raw);
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
 };
 
 #endif
