23dc8dad049189311d21df5a6ea69ce422aecc2f
Harsh Kumbhani <Harshwut@icloud.com>
2024-04-06 10:51:47 +0200

    Finished main test for ex03 module05

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -6,14 +6,13 @@
 #include "ShrubberyCreationForm.hpp"
 #include "Utils.hpp"
 
-void  test_shrubbery_form() {
+void test_shrubbery_form() {
 
   std::cout << color("\n========================================\n", BLUE);
-  std::cout << color("      SHRUBBERY CREATION TRAINING \n",
-                     YELLOW);
+  std::cout << color("      SHRUBBERY CREATION FORM TRAINING \n", YELLOW);
   std::cout << color("========================================\n", BLUE);
-  Bureaucrat  Espresso_Patronum("Espresso_Patronum", 3);
-  Intern  Pixel_Waffle;
+  Bureaucrat Espresso_Patronum("Espresso_Patronum", 3);
+  Intern Pixel_Waffle;
   AForm *shrubbey = Pixel_Waffle.makeForm("Shrubbery Creation", "MARS");
 
   Espresso_Patronum.signForm(*shrubbey);
@@ -21,13 +20,12 @@ void  test_shrubbery_form() {
   delete shrubbey;
 }
 
-void  test_robotomy_form() {
+void test_robotomy_form() {
   std::cout << color("\n========================================\n", BLUE);
-  std::cout << color("      ROBOTOMY REQUEST TRAINING\n",
-                     YELLOW);
+  std::cout << color("      ROBOTOMY REQUEST FORM TRAINING\n", YELLOW);
   std::cout << color("========================================\n", BLUE);
-  Bureaucrat  Cypher_Doc("Cypher_Doc", 3);
-  Intern  Palette_Turner;
+  Bureaucrat Cypher_Doc("Cypher_Doc", 3);
+  Intern Palette_Turner;
   AForm *robotomy = Palette_Turner.makeForm("Robotomy Request", "42 GPT");
 
   Cypher_Doc.signForm(*robotomy);
@@ -35,48 +33,48 @@ void  test_robotomy_form() {
   delete robotomy;
 }
 
-void  test_presidential_pardon_form() {
+void test_presidential_pardon_form() {
   std::cout << color("\n========================================\n", BLUE);
-  std::cout << color("      PRESIDENTAIL PARDON  TRAINING\n",
-                     YELLOW);
+  std::cout << color("      PRESIDENTAIL PARDON FORM TRAINING\n", YELLOW);
   std::cout << color("========================================\n", BLUE);
-  Bureaucrat  Cypher_Doc("Cypher_Doc", 3);
-  Intern  Palette_Turner;
-  AForm *pardon = Palette_Turner.makeForm("Presidential Pardon", "42 GPT");
+  Bureaucrat justus("Justus Fairway", 3);
+  Intern Page_Turner;
+  AForm *pardon = Page_Turner.makeForm("Presidential Pardon", "Mercu Fuller");
 
-  Cypher_Doc.signForm(*pardon);
-  Cypher_Doc.executeForm(*pardon);
+  justus.signForm(*pardon);
+  justus.executeForm(*pardon);
   delete pardon;
 }
 
-void  test_non_existing_form() {
+void test_non_existing_form() {
   std::cout << color("\n========================================\n", BLUE);
-  std::cout << color("      NON EXISTING  TRAINING\n",
-                     YELLOW);
+  std::cout << color("      NON EXISTING FORM TRAINING\n", YELLOW);
   std::cout << color("========================================\n", BLUE);
-  Bureaucrat  Cypher_Doc("Cypher_Doc", 3);
-  Intern  Palette_Turner;
-  AForm *pardon = Palette_Turner.makeForm("Hi i don't Exist", "42 GPT");
+  Bureaucrat Cypher_Doc("Cypher_Doc", 3);
+  Intern Palette_Turner;
 
-  Cypher_Doc.signForm(*pardon);
-  Cypher_Doc.executeForm(*pardon);
-  delete pardon;
+  try {
+    AForm *pardon = Palette_Turner.makeForm("Hi i don't Exist", "42 GPT");
+    Cypher_Doc.signForm(*pardon);
+    Cypher_Doc.executeForm(*pardon);
+    delete pardon;
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
 }
 
 int main() {
   std::cout << color("\n========================================\n", BLUE);
-  std::cout << color("      Intern training starts\n",
-                     YELLOW);
+  std::cout << color("      Intern training starts\n", YELLOW);
   std::cout << color("========================================\n", BLUE);
 
   test_shrubbery_form();
   test_robotomy_form();
   test_presidential_pardon_form();
-  // test_non_existing_form();
+  test_non_existing_form();
 
   std::cout << color("\n========================================\n", BLUE);
-  std::cout << color("  Intern training Ends\n",
-                     YELLOW);
+  std::cout << color("  Intern training Ends\n", YELLOW);
   std::cout << color("========================================\n", BLUE);
   std::cout << std::endl;
   return 0;
