7759e99de34d34db29229ebfd08f3a4f9ea838d9
Harsh Kumbhani <Harshwut@icloud.com>
2024-04-09 11:34:30 +0200

    formatted main

--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -1,21 +1,17 @@
 #include "Base.hpp"
 #include "Functions.hpp"
 
-int main () {
+int main() {
 
   std::cout << "Identifying using pointers" << std::endl;
   Base *p = generate();
-
   identify(p);
-
-  std::cout << "Identifying using References" << std::endl;
-
   delete p;
-
+  
+  std::cout << "Identifying using References" << std::endl;
   Base *q = generate();
   identify(*q);
-
   delete q;
+
   return 0;
 }
-
