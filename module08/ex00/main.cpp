0b72511d6c43fae7a32fb94dbe9f7d9590ea9ef9
Harsh Kumbhani <Harshwut@icloud.com>
2024-04-13 19:51:50 +0200

    added some tests

--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -26,26 +26,26 @@ void test_find_in_container(const std::string &description, int numToSearch,
 }
 
 int main() {
-  int numsVector[] = {8, 4, 5, 9};
+  int numsVector[] = {5, 3, 17, -2, 9, 11};
   std::vector<int> vector1(numsVector, numsVector + sizeof(numsVector) /
                                                         sizeof(numsVector[0]));
   std::vector<int> vector2(numsVector, numsVector + sizeof(numsVector) /
                                                         sizeof(numsVector[0]));
   vector2.push_back(42);
 
-  int numsList[] = {8, 4, 5, 9};
+  int numsList[] = {5 ,3, 17, -2, 9, 11};
   std::list<int> list1(numsList,
                        numsList + sizeof(numsList) / sizeof(numsList[0]));
   std::list<int> list2(numsList,
                        numsList + sizeof(numsList) / sizeof(numsList[0]));
   list2.push_back(42);
 
-  test_find_in_container("Find number in vector.", 5, vector1);
+  test_find_in_container("Find number in vector.", -2, vector1);
   test_find_in_container("Number not found in vector.", 42, vector1);
   test_find_in_container("Find number in vector.", 42, vector2);
   test_find_in_container("Number not found in vector.", 100, vector2);
 
-  test_find_in_container("Find number in list.", 5, list1);
+  test_find_in_container("Find number in list.", -2, list1);
   test_find_in_container("Number not found in list.", 42, list1);
   test_find_in_container("Find number in list.", 42, list2);
   test_find_in_container("Number not found in list.", 100, list2);
